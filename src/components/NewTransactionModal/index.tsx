2455dfbc440871f06b688f497ed4568697da5b49
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2022-09-05 23:19:27 -0300

    perf

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,8 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as Dialog from '@radix-ui/react-dialog'
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
-import { useContext } from 'react'
+
+import { useContextSelector } from 'use-context-selector'
 
 import { Controller, useForm } from 'react-hook-form'
 import * as z from 'zod'
@@ -25,7 +26,12 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionModal() {
-  const { createTransaction } = useContext(TransactionsContext)
+  const createTransaction = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.createTransaction
+    },
+  )
 
   const {
     control,
